ce24526988b6aaf03ab9bacebc4b9eee86efee9b
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-07-29 09:14:21 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './styles/App.css'
+import Header from  './header.jsx'
+import Body from './body.jsx'
+import Profile from './profile.jsx'
+
+function App() {
+  return (
+    <>
+<div>
+<Header/>
+<Body/>
+</div>
+<Profile/>
+    </>
+  )
+}
+
+export default App
